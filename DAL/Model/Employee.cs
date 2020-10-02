eeae2a1704c35118138e4c6c907498f8423cd71b
AnizioCunha <robsonsouzamao@gmail.com>
2020-10-02 03:27:09 -0400

    Commit inicial com a classe DAL

---
+++ DAL/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int Id_rule { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public Sexo Gender { get; set; }
+        public IsAtivo Active { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Modified_at { get; set; }
+    }
+}
---
+++ DAL/Model/Rule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    //inicio da classse Rule
+    public class Rule
+    {
+        //atributos
+        public int Id { get; set; }
+        public string name { get; set; }
+        public IsAtivo Active { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Modified_at { get; set; }
+
+    } // fim classe Rule
+}
